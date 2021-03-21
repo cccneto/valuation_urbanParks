89ce385b94adda9b249f72f45c38780f630c3da5
Claudiano Neto <cneto@ufrb.edu.br>
2021-03-21 12:09:58 -0300

    adicionando regressoes para doble-bounded

--- teste.R
+++ teste.R
@@ -4,6 +4,7 @@ library(Ecdat)
 library(lmtest)
 library(dplyr)
 library(VIM)
+library(GJRM)
 
 # carregando base
 url <-
@@ -17,16 +18,24 @@ baseunificada <- read_excel(destfile)
 
 baseunificada <- baseunificada %>%
   mutate(
-    infraestrutura = case_when(
+    infraestrutura = case_when( # dummie para bom/otimo = 1
       infraestrutura == '1' ~ "0",
       infraestrutura == '2' ~ "0",
       infraestrutura == '3' ~ "0",
-      TRUE ~ "1"
-    )
-  ) %>%
-  mutate(escolar = case_when(escolar == '16' ~ "1",
-                             TRUE ~ "0")) %>% # 1=superior completo, 0= otherwise
-  mutate(sexo = case_when(sexo == '2' ~ "0",
+      TRUE ~ "1"), 
+    temperatura = case_when(  # dummie para bom/otimo = 1
+          temperatura == '1' ~ "0",
+          temperatura == '2' ~ "0",
+          temperatura == '3' ~ "0",
+          TRUE ~ "1"),
+     sombra = case_when( # dummie para bom/otimo = 1
+              sombra == '1' ~ "0",
+              sombra == '2' ~ "0",
+              sombra == '3' ~ "0",
+              TRUE ~ "1"), 
+     escolar = case_when(escolar == '16' ~ "1",
+                             TRUE ~ "0"), # 1=superior completo, 0= otherwise
+     sexo = case_when(sexo == '2' ~ "0",
                           sexo == '3' ~ "0",
                           TRUE ~ "1")) %>%
   glimpse()
@@ -35,27 +44,9 @@ baseunificada <- baseunificada %>%
 dados <- baseunificada %>%
   filter(!is.na(baseunificada)) %>% # filtrando missing values
   filter(!lance2 < 1) %>% # retirar os zeros do lance2
-  select(
-    parque,
-    idade,
-    sexo,
-    cidade,
-    escolar,
-    renda,
-    qdepend,
-    objetivo,
-    infraestrutura,
-    sombra,
-    temperatura,
-    tamanho,
-    lance1,
-    lance2,
-    depend,
-    resp1,
-    resp2,
-    bidl,
-    bidh
-  ) %>%
+  select( parque, idade, sexo, cidade, escolar,renda, qdepend, 
+          objetivo, infraestrutura, sombra, temperatura, tamanho,
+           lance1,lance2, depend, resp1, resp2, bidl, bidh) %>%
   glimpse()
 
 dados <- dados %>%
@@ -67,7 +58,7 @@ dados <- dados %>%
     escolar = as.factor(escolar),
     renda = as.integer(renda),
     qdepend = as.integer(qdepend),
-    depend = as.factor(depend),
+    depend = as.double(depend),
     objetivo = as.factor(objetivo),
     infraestrutura = as.numeric(infraestrutura),
     lance1 = as.integer(lance1),
@@ -102,122 +93,70 @@ with(
   )
 )
 
-View(dados)
 # testing correlation between variables
 dados %>%
   select(resp1, resp2, lance1, lance2) %>%
   cor()  # conclusao: dado que $\rho$ != 0, vamos utilizar pelo modelo bivariado
 
-# testing variables
-sb1 <-  sbchoice(resp1 ~ idade | lance1, dist = "logistic", data = dados)
+# testing variables to all parks
+sb1 <-  sbchoice(resp1 ~ idade | lance1, dist = "logistic", data = dados) # NSS
 sb2 <-  sbchoice(resp1 ~ sexo | lance1, dist = "logistic", data = dados) # NSS
 sb3 <-  sbchoice(resp1 ~ renda | lance1, dist = "logistic", data = dados)
 sb4 <-  sbchoice(resp1 ~ escolar | lance1, dist = "logistic", data = dados) # NSS
 sb5 <-  sbchoice(resp1 ~ infraestrutura | lance1, dist = "logistic", data = dados)
 sb6 <-  sbchoice(resp1 ~ qdepend | lance1, dist = "logistic", data = dados)
-sb7 <-  sbchoice(resp1 ~ depend | lance1, dist = "logistic", data = dados)
+sb7 <-  sbchoice(resp1 ~ depend | lance1, dist = "logistic", data = dados)  # NSS
 
-summary(sb1)
-summary(sb2)
-summary(sb3)
-summary(sb4)
-summary(sb5)
-summary(sb6)
-
-
-
-# plots the predicted support at each bid value
-plot(sb1, las = 1)    #  las control axis orientation
-abline(h = 0.5, lty = 2, col = "red") # adds a horizontal line to the plot
+# saving data from each Park - the full file that contains all data is 'dados'.
+dados_sitio_da_trindade <- dados %>% filter(parque == "sitio da trindade")
+dados_13demaio <- dados %>% filter(parque == "13demaio")
+dados_santosdumont <- dados %>% filter(parque == "santosdumont")
+dados_lindu <- dados %>% filter(parque == "lindu")
+dados_caiara <- dados %>% filter(parque == "caiara")
+dados_macaxeira <- dados %>% filter(parque == "macaxeira")
+dados_jaqueira <- dados %>% filter(parque == "jaqueira")
+dados_santana <- dados %>% filter(parque == "santana")
 
 
-# A relatively complete model
-sb1 <- sbchoice(resp1 ~ 1 | lance1, dist = "logistic", data = dados)
-summary(sb1)
+# # plots the predicted support at each bid value
+# plot(sb1, las = 1)    #  las control axis orientation
+# abline(h = 0.5, lty = 2, col = "red") # adds a horizontal line to the plot
+# 
+# 
+# # A relatively complete model
+# sb1 <- sbchoice(resp1 ~ 1 | lance1, dist = "logistic", data = dados)
+# summary(sb1)
 
-# get confidence intervals for the model sb1
-set.seed(123) # The start value can be anything
-krCI(sb1)     # The command to get the relevant 95% CI by Krinsky Robb method
+# set.seed(123) # As it is a new simulation we again set a start value
+# bootCI(sb1)   # The command to get the relevant 95% CI by bootstrap method
 
-set.seed(123) # As it is a new simulation we again set a start value
-bootCI(sb1)   # The command to get the relevant 95% CI by bootstrap method
-
-# melhor ajuste para sbdc
+# # melhor ajuste para sbdc
 sb.full <- sbchoice(resp1 ~ 1 + idade | lance1, dist = "logistic",
                     data = dados)
 
+# # get confidence intervals for the model sb.full
+# set.seed(123) # We still need a start value for the simulation
+# bootCI(sb.full)
+# bootCI(sb1)
 
-# get confidence intervals for the model sb.full
-set.seed(123) # We still need a start value for the simulation
-bootCI(sb.full)
-bootCI(sb1)
+## CLASSIC BIVARIATE PROBIT
 
-## AN INITIAL GENERAL DOUBLE BOUNDED MODEL
+# pela AIC curve 'out1' tem melhor ajuste
+out1 <- gjrm(list(resp1 ~ lance1 + idade + sexo + renda + escolar + infraestrutura + qdepend + depend,
+                  resp2 ~ lance2 + idade + sexo + renda + escolar + infraestrutura + qdepend + depend), 
+             data = dados, margins = c("probit", "probit"), Model = "B")
 
-db.full <-
-  dbchoice(
-    resp1 + resp2 ~ 1 |
-      lance1 + lance2,
-    dist = "logistic",
-    log = TRUE,
-    data = dados
-  )
+out2 <- gjrm(list(resp1 ~ lance1 + idade + sexo + renda + escolar + infraestrutura + qdepend,
+                  resp2 ~ lance2 + idade + sexo + renda + escolar + infraestrutura + qdepend), 
+             data = dados, margins = c("probit", "probit"), Model = "B")
 
-#criando variavel para join "NP" e "dados"
-NP$ID <- seq(1:length(NP$age))
-
-dados$ID <- seq(1:length(dados$parque))
-basecompleta <- left_join(NP, dados, by = "ID")
+out3 <- gjrm(list(resp1 ~ lance1 + idade + sexo + renda + escolar + infraestrutura,
+                  resp2 ~ lance2 + idade + sexo + renda + escolar + infraestrutura), 
+             data = dados, margins = c("probit", "probit"), Model = "B")
 
 
+out4 <- gjrm(list(resp1 ~ lance1 + idade + sexo + escolar + infraestrutura,
+                  resp2 ~ lance2 + idade + sexo + escolar + infraestrutura), 
+             data = dados, margins = c("probit", "probit"), Model = "B")
 
 
-# testando nova estrutura de dados
-
-base_df.full <-
-  dados %>% select(
-    resp1,
-    resp2,
-    idade,
-    sexo,
-    estadocivil,
-    qdepend,
-    renda,
-    infraestrutura,
-    lance1,
-    lance2,
-    bidl,
-    bidh
-  )
-
-teste_df.full <- teste_df.full %>%
-  mutate(nn = case_when(resp1 == '0' &
-                          resp2 == '0'  ~ 1, TRUE ~ 0)) %>%
-  mutate(ny = case_when(resp1 == '0' &
-                          resp2 == '1'  ~ 1, TRUE ~ 0)) %>%
-  mutate(yy = case_when(resp1 == '1' &
-                          resp2 == '1'  ~ 1, TRUE ~ 0)) %>%
-  mutate(yn = case_when(resp1 == '1' & resp2 == '0'  ~ 1, TRUE ~ 0))
-
-teste_df.full <- dados %>% select(resp1, resp2, lance1, bidl, bidh)
-
-teste <-
-  ct2df(
-    teste_df.full,
-    bid1 = "lance1",
-    bid2h = "bidh",
-    bid2l = "bidl",
-    yy = "yy",
-    yn = "yn",
-    ny = "ny",
-    nn = "nn",
-    y = "y",
-    n = "n",
-    type = "double"
-  )
-
-base_df.full %>% glimpse()
-
-dados %>% summary()
-
-dados %>% select(lance2) %>% filter(lance2 == 0)
